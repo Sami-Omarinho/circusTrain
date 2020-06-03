a7e8e7ddf0241e1687e59b2a7c637dacf85918fb
Omar Kacimi <Okacimi1999@gmail.com>
2020-06-03 12:20:19 +0200

    assing waggon and xml
    renamend wagon to assign wagon and build the functions to show wagons with animals.
    worked on xml and updated functions

---
+++ CircusTrain/AssignWagon.cs
@@ -0,0 +1,40 @@
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CircusTrain
+{
+    public class AssignWagon
+    {
+        // A list of all the wagons created.
+        public readonly List<Wagon> WagonList = new List<Wagon>();
+
+        // List of animals that get put in wagons.
+        private readonly List<Animal> _animalList;
+
+        // list to fill with animal object
+        public AssignWagon(List<Animal> animals)
+        {
+            _animalList = animals;
+        }
+
+        // Distributes all animals in the List in wagons
+        // Filled wagons get put in list
+        public void assignWagon()
+        {
+            foreach (var animal in _animalList)
+            {
+                if (WagonList.Any(wagon => wagon.AddAnimalToWagon(animal))) continue;
+
+                AddWagonWithAnimal(animal);
+            }
+        }
+        
+        // Creates a new wagon and adds the animal to the newly created wagon.
+        private void AddWagonWithAnimal(Animal animal)
+        {
+            var newWagon = new Wagon();
+            newWagon.AddAnimalToWagon(animal);
+            WagonList.Add(newWagon);
+        }
+    }
+}
